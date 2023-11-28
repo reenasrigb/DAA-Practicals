2c0d0168ea654ebcd2b7c9a9ea72520ee5e985d3
reenasrigb <122506938+reenasrigb@users.noreply.github.com>
2023-11-28 08:36:41 +0530

    Add files via upload

---
+++ 1.targer(set3).cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int binarySearch(int nums[], int size, int target) {
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    int nums[] = { -1, 0, 3, 5, 9, 12 };
+    int size = sizeof(nums) / sizeof(nums[0]);
+    int target = 9;
+
+    int result = binarySearch(nums, size, target);
+
+    if (result != -1) {
+        printf("Target %d found at index %d\n", target, result);
+    } else {
+        printf("Target %d not found in the array\n", target);
+    }
+
+    return 0;
+}
